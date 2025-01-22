4ebbdd1e16f13437b8d5acd7bba7b31d6f6bcb38
yuk14 <152795561+yuk14@users.noreply.github.com>
2025-01-22 09:03:52 +0530

    Update urls.py

--- document_analyzer/urls.py
+++ document_analyzer/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from analyzer import views
-from django.conf import settings
-from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
         path('', RedirectView.as_view(url='upload/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('upload/', views.upload_document, name='upload_document'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
